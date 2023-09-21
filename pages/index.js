152a5a4938d9b74263c3b39ded90ddf1c4a6b4c9
Javier Landini <javier.landini@agileengine.com>
2023-09-21 15:41:29 -0300

    Make pieces start falling point random. Add game speed acceleration based on points earned.

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import { useEffect, useState } from "react";
 class Tetris {
   #X_BOARD;
   #Y_BOARD;
+  MAX_SPEED;
   board;
   piece;
   score;
   gameOver;
+  speed;
   constructor() {
     this.#X_BOARD = 10;
     this.#Y_BOARD = 20;
@@ -45,14 +47,15 @@ class Tetris {
 
     this.score = 0;
     this.gameOver = false;
-
+    this.speed = 0;
+    this.MAX_SPEED = 4;
     this.generatePiece();
   }
 
   generatePiece() {
     const shape = this.SHAPES[Math.floor(Math.random() * this.SHAPES.length)];
     this.piece = {
-      x: this.#X_BOARD / 2,
+      x: Math.floor(Math.random() * (this.#X_BOARD - shape[0].length + 1)),
       y: 0,
       shape,
     };
@@ -159,15 +162,31 @@ class Tetris {
       lines++;
     }
     this.score = this.score + lines * lines * 100;
+    this.speed =
+      Math.floor(this.score / 1000) > this.MAX_SPEED
+        ? 4
+        : Math.floor(this.score / 1000);
   }
 }
 const tetris = new Tetris();
 
 export default function Home() {
   const [_, setState] = useState({});
+  const [counter, setCounter] = useState(tetris.MAX_SPEED);
+  const container_style = {
+    backgroundColor:
+      tetris.speed == 0
+        ? "white"
+        : tetris.speed == 1
+        ? "antiquewhite"
+        : tetris.speed == 2
+        ? "navajowhite"
+        : tetris.speed == 3
+        ? "peachpuff"
+        : "mediumpurple",
+  };
   useEffect(() => {
     const handleKeyDown = (e) => {
-      console.log(e);
       e.preventDefault();
       if (tetris.gameOver) {
         return;
@@ -195,8 +214,25 @@ export default function Home() {
     return () => window.removeEventListener("keydown", handleKeyDown);
   });
 
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (!tetris.gameOver) {
+        if (counter === tetris.speed) {
+          // Reset counter.
+          setCounter(tetris.MAX_SPEED);
+          tetris.movePiece(0, 1);
+        } else {
+          setCounter(counter - 1);
+        }
+      }
+      setState({});
+    }, 100);
+
+    return () => clearInterval(id);
+  }, [counter]);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={container_style}>
       <Head>
         <title>Tetris example</title>
         <link rel="icon" href="/favicon.ico" />
